b476d721500033324dc6cf98ea4a43294da21cc8
Eisklaue <eisklaue@gmail.com>
2015-05-07 22:44:22 +0200

    Version 1.0

---
+++ OrbitHud/DisplayItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace OrbitHud
+{
+    class DisplayItem
+    {
+        public string label;
+        public string value;
+
+        public GUIStyle valueStyle = new GUIStyle();
+        public GUIStyle labelStyle = new GUIStyle();
+
+        public DisplayItem (string label)
+        {
+            this.label = label;
+
+            this.valueStyle.normal.textColor = Color.green;
+            this.valueStyle.alignment = TextAnchor.MiddleLeft;
+            this.valueStyle.padding.left = 10;
+
+            this.labelStyle.normal.textColor = Color.green;
+            this.labelStyle.alignment = TextAnchor.MiddleRight;
+        }
+    }
+}
